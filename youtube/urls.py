d223bf731227f1f898c29098b6eda388242869eb
Ashutosh Pradhan <ashutoshpradhan164@gmail.com>
2022-03-20 21:16:46 +0530

    templates working

--- youtube/urls.py
+++ youtube/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from .views import index
 
 
 urlpatterns = [
-    
+    path('', index, name='index'),
 ]
--- youtube/views.py
+++ youtube/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from pytube import *
+from django.contrib import messages
 
-# Create your views here.
+
+def index(request):
+    # if request.method == 'POST':
+    #     url = request.POST['url']
+    #     video = YouTube(url)
+    #     stream = video.streams.get_by_resolution()
+    #     stream.download()
+    #     messages.info(request, 'Video Downloaded........')
+    #     return render(request, 'index.html')
+    return render(request, 'index.html')
+
+
+
+
+
+
+

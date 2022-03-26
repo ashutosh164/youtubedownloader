39b901696b07e43527a8da3ae97c5b7dead5aa0e
Ashutosh Pradhan <ashutoshpradhan164@gmail.com>
2022-03-26 19:16:53 +0530

    remove some extra line

--- youtube/views.py
+++ youtube/views.py
@@ -6,13 +6,11 @@ from django.contrib import messages
 def index(request):
     if request.method == 'POST':
         url = request.POST.get('url')
-        print(url)
         video = YouTube(url)
         stream = video.streams.get_highest_resolution()
-
         stream.download()
         messages.info(request, 'Video Downloaded........')
-        return render(request, 'index.html', {'url':url})
+        return render(request, 'index.html', {'url': url})
     return render(request, 'index.html')
 
 

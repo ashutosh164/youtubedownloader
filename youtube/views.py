9dbb5298d3a8020e67aef760f34de8042aab318a
Ashutosh Pradhan <ashutoshpradhan164@gmail.com>
2022-03-27 15:41:17 +0530

    programm run while url is not filled

--- youtube/views.py
+++ youtube/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from pytube import *
 from django.contrib import messages
 
@@ -6,10 +6,14 @@ from django.contrib import messages
 def index(request):
     if request.method == 'POST':
         url = request.POST.get('url')
+        if url == '':
+            messages.error(request, 'Please enter a valid URL..')
+            return redirect('/')
         video = YouTube(url)
         stream = video.streams.get_highest_resolution()
+        messages.info(request, 'Video Downloading please wait.....')
         stream.download()
-        messages.info(request, 'Video Downloaded........')
+        messages.info(request, 'Video Downloaded Successfully ........')
         return render(request, 'index.html', {'url': url})
     return render(request, 'index.html')
 
